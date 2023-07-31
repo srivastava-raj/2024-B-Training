8f8453ea36495f2bdf61aa55cf60f1af16173caa
Karnik22 <93015384+Karnik22@users.noreply.github.com>
2023-08-01 02:58:00 +0530

    Update 12. Sum of all Subarrays.py

--- 2000290120083/Arrays/12. Sum of all Subarrays.py
+++ 2000290120083/Arrays/12. Sum of all Subarrays.py
@@ -1,19 +1,6 @@
 # https://www.geeksforgeeks.org/sum-of-all-subarrays/
 
 
-'''
-Given an integer array ‘arr[]’ of size N, find the sum of all sub-arrays of the given array. 
-
-Input: arr[] = {1, 2, 3}
-Output: 20
-Explanation: {1} + {2} + {3} + {2 + 3} + {1 + 2} + {1 + 2 + 3} = 20
-
-Input: arr[] = {1, 2, 3, 4}
-Output: 50
-'''
-
-
-
 def SubArraySum(arr, n):
     result = 0
  
