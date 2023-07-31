ec11852a7d00aea8f27e1024430065339b3d5ae9
Karnik22 <93015384+Karnik22@users.noreply.github.com>
2023-08-01 02:53:52 +0530

    Update 02. Split Array Largest Sum.py

--- 2000290120083/Binary Search/02. Split Array Largest Sum.py
+++ 2000290120083/Binary Search/02. Split Array Largest Sum.py
@@ -2,21 +2,19 @@
 
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
-        # First Solve => Allocate Minimum Number Of Pages - Binary Search
         
         l = max(nums);  r = sum(nums);  ans = -1
         
-        if len(nums) < m: return -1  # Number of elements can not be lesser than number of subarrays as we have to give atleast 1 element to a subarray
-
+        if len(nums) < m: return -1  
         def isValid(nums, m, mid):
-            currSum = 0              # sum of elements of nums that can be allocated to one subarray
-            countOfSubarrays = 1     # Number of subarrays required if mid is the max capacity of a subarray
+            currSum = 0             
+            countOfSubarrays = 1     
             
             for val in nums:
                 currSum += val
-                if currSum > mid:          # sum of elements allocated to one subarray exceed max capacity of the subarray
-                    countOfSubarrays += 1  # We need one more subarray 
-                    currSum = val          # start calculating sum of elements that can be allocated to next subarray
+                if currSum > mid:          
+                    countOfSubarrays += 1  
+                    currSum = val          
             
             if countOfSubarrays > m: return False
             else: return True
@@ -25,12 +23,12 @@ class Solution:
         while l <= r:
             mid = (r+l) // 2
             if isValid(nums, m, mid):
-                ans = mid            # Updating answer to current mid as current mid is the most optimized(least) ans till now
-                r = mid - 1          # I will try to decrease mid
+                ans = mid          
+                r = mid - 1        
             else:
-                l = mid + 1          # current mid NOT isValid so I will try to increase mid
+                l = mid + 1        
         
-        return ans                   # Most Optimized ans is stored here
+        return ans                   
             
             
             
